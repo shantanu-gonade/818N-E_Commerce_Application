9bfc19f45abbed10ade503b6ff6d654189af0430
shantanu341 <43992321+shantanu-gonade@users.noreply.github.com>
2025-03-23 17:30:06 -0400

    Testing db connection

--- includes/.env.php
+++ includes/.env.php
@@ -1,11 +1,9 @@
 <?php
-// Database connection parameters
-// These values will be populated by the EC2 user data script
-// from AWS Secrets Manager
-
-define('DB_HOST', '');     // Will be populated with RDS endpoint
-define('DB_USER', '');     // Will be populated with database username
-define('DB_PASS', '');     // Will be populated with database password
-define('DB_NAME', '');     // Will be populated with database name
-define('DB_SSL', true);    // Enable SSL for secure connection
+// Database connection environment variables
+// These will be populated by EC2 userdata script
+$_ENV['DB_HOST'] = ''; // Will be populated with RDS endpoint
+$_ENV['DB_USER'] = ''; // Will be populated with database username
+$_ENV['DB_PASSWORD'] = ''; // Will be populated with database password
+$_ENV['DB_NAME'] = ''; // Will be populated with database name
+$_ENV['DB_SSL'] = ''; // Will be populated with SSL setting (true/false)
 ?>
